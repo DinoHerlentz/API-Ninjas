94683b7828fcafb2e76f83afed72303012695600
DinoHerlentz <87923606+DinoHerlentz@users.noreply.github.com>
2023-11-08 10:16:02 +0700

    Update facts.py

--- API Ninjas/facts.py
+++ API Ninjas/facts.py
@@ -3,7 +3,7 @@ import json
 
 limit = 1
 api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
-response = requests.get(api_url, headers={'X-Api-Key': 'Yhl7iIvZsSp+Z1wgz7IClw==elM4rnguiSPIrIRI'})
+response = requests.get(api_url, headers={'X-Api-Key': os.environ['NINJA']})
 
 if response.status_code == requests.codes.ok:
     data = json.loads(response.text)
