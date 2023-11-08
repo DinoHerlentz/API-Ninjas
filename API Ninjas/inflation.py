649a611d718a956937def967fa3711358f063d4e
DinoHerlentz <87923606+DinoHerlentz@users.noreply.github.com>
2023-11-08 10:16:13 +0700

    Update inflation.py

--- API Ninjas/inflation.py
+++ API Ninjas/inflation.py
@@ -3,7 +3,7 @@ import json
 
 country = input("Enter Country Name : ")
 api_url = 'https://api.api-ninjas.com/v1/inflation?country={}'.format(country)
-response = requests.get(api_url, headers={'X-Api-Key': 'Yhl7iIvZsSp+Z1wgz7IClw==elM4rnguiSPIrIRI'})
+response = requests.get(api_url, headers={'X-Api-Key': os.environ['NINJA']})
 
 if response.status_code == requests.codes.ok:
     data = json.loads(response.text)
