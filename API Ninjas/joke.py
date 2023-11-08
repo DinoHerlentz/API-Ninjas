691c064967029a0bc1009d6dcc94019565c654d1
DinoHerlentz <87923606+DinoHerlentz@users.noreply.github.com>
2023-11-08 10:16:37 +0700

    Update joke.py

--- API Ninjas/joke.py
+++ API Ninjas/joke.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 api_url = 'https://api.api-ninjas.com/v1/jokes?limit=1'
-response = requests.get(api_url, headers={'X-Api-Key': 'Yhl7iIvZsSp+Z1wgz7IClw==elM4rnguiSPIrIRI'})
+response = requests.get(api_url, headers={'X-Api-Key': os.environ['NINJA']})
 if response.status_code == requests.codes.ok:
     data = json.loads(response.text)
 
